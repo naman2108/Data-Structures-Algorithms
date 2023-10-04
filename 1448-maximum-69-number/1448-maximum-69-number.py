710dc8c1eceed2f5bf846616fa305342a763f409
Naman Sharma <76205377+naman2108@users.noreply.github.com>
2023-10-05 01:22:09 +0530

    Time: 41 ms (31.96%), Space: 16.1 MB (77.59%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        arr=[]
+        nums=str(num)
+        for i in nums:
+            arr.append(i)
+        for i in range(len(arr)):
+            if arr[i]=='6':
+                arr[i]='9'
+                break
+        return int(''.join(arr))            
+       
+        
+                    
+        
