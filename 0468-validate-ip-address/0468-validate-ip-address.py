8a8272629a3b4a59032c65aca020845b1bb9ff77
Naman Sharma <76205377+naman2108@users.noreply.github.com>
2023-10-21 10:28:34 +0530

    Time: 41 ms (35.82%), Space: 16.2 MB (66.8%) - LeetHub

--- 0468-validate-ip-address/0468-validate-ip-address.py
+++ 0468-validate-ip-address/0468-validate-ip-address.py
@@ -1,42 +1,40 @@
 class Solution:
-    def ipv4(self,s):
-        s=s.split('.')
-        if len(s)!=4:
+    def ipv4(self, queryIP):
+        s = queryIP.split('.')
+        if len(s) != 4:
             return False
         for i in s:
             if not i.isdigit():
                 return False
-            if int(i)<0 or int(i)>255:
+            if int(i) < 0 or int(i) > 255:
                 return False
-            if i.startswith('0') and len(i)>1:
+            if len(i) > 1 and i[0] == '0':
                 return False
         return True
-    def ipv6(self,s):
-        mp=set('0123456789ABCDEFabcdef')
-        s=s.split(':')
-        if len(s)!=8:
+
+    def ipv6(self, queryIP):
+        s = queryIP.split(':')
+        if len(s) != 8:
             return False
+        validchar = set('0123456789abcdefABCDEF')
         for i in s:
-            if not i or  len(i)>4:
+            
+            if not i or len(i) > 4:
                 return False
             for c in i:
-                if c not in mp:
-                    return False
-                    
-        return True                    
+                if c not in validchar:
+                    return False    
+        return True
 
-
-
-
-                                        
     def validIPAddress(self, queryIP: str) -> str:
-        if ':' in queryIP:
-            if self.ipv6(queryIP):
-                return 'IPv6'
-        elif '.' in queryIP:
-            if self.ipv4(queryIP):
-                return 'IPv4'
+        if self.ipv4(queryIP):
+            return 'IPv4'
+        elif self.ipv6(queryIP):
+            return 'IPv6'
         
-        return 'Neither'                
-               
+        return 'Neither'
+    
+
+
+
         
