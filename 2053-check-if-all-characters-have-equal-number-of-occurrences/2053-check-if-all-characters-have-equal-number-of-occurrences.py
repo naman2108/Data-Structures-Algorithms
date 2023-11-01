8a55511015486f1a520a2d0124e8d91531db95e8
Naman Sharma <76205377+naman2108@users.noreply.github.com>
2023-11-01 13:59:30 +0530

    Time: 41 ms (62.01%), Space: 16.2 MB (56.98%) - LeetHub

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -6,12 +6,9 @@ class Solution:
         mp = defaultdict(int)
         for i in s:
             mp[i] += 1
-        first_count = None
-        for i in mp:
-            if first_count is None:
-                first_count = mp[i]
-            elif mp[i] != first_count:
+        for i,j in mp.items():
+            if j!=mp[s[0]]:
                 return False
-        return True
-                
+        return True        
+
         
